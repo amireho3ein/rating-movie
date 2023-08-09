6aaa17568d299666739a47fbcf9aefa441ed032b
amireho3ein <amireedoran@gmail.com>
2023-08-09 12:11:41 +0330

    Code refactoring and code cleaning

--- imdb_rating.py
+++ imdb_rating.py
@@ -1,10 +1,15 @@
 import imdb
-def mov(name_mov):
-    ia = imdb.IMDb()
-    movie_search = ia.search_movie(name_mov)
-    id_movie = movie_search[0].movieID
-    series = ia.get_movie(id_movie)
-    rating = series.data['rating']
-    return f"rating {name_mov} : {rating}"
 
-print(mov(str(input("Name the movie you want to rate:  "))))
+def get_movie_rating(movie_title):
+    """
+    This function takes in a movie title and returns its rating using the IMDb library.
+    """
+    ia = imdb.IMDb()
+    movie_search = ia.search_movie(movie_title)
+    movie_id = movie_search[0].movieID
+    movie = ia.get_movie(movie_id)
+    rating = movie.data['rating']
+    return f"The rating of '{movie_title}' is: {rating}"
+    
+movie_title = input("Enter the movie title: ")
+print(get_movie_rating(movie_title))
